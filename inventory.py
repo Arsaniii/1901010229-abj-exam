2fee1d276744e6921605d68e26e77e8405091f38
Arsaniii <changeme@example.com>
2022-04-22 05:05:16 +0000

    program python untuk manajemen inventory

---
+++ inventory.py
@@ -0,0 +1,16 @@
+file = open ("db-inventory.txt","a")
+while True:
+    tidak = input ("input data inventory baru ? (ya/tidak)")
+    if tidak == 'tidak' or tidak == 'Tidak':
+        file = open ("db -inventory.txt","r")
+        for item in file:
+            item=item.strip()
+            print(item)
+        break
+    elif tidak == 'ya' or tidak == 'Ya':
+        print("------------------------------")
+        x = input ("Masukkan nama perangkat : ")
+        y = input ("Masukkan lokasi : ")
+        file.write ("nama perangkat : " +x + ", lokasi : "+ y +"/n")
+        print("--------------------------------")
+    file.close()
